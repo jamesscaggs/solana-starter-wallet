6070cefebfe5b0af4470d3876563d3dc45ca9990
JAMES SCAGGS <james.scaggs@gmail.com>
2021-08-24 21:44:32 -0500

    added navbar, layout component, and links

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import Head from "next/head";
+import Layout from "../components/layout";
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    <Layout>
+      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <Head>
+          <title>Create Next App</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
 
-      <div>
-        <button>Send</button>
+        <div>
+          <button>Send</button>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
+
+Home.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
+};
